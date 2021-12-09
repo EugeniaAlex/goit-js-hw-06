505b1a9d06414e30143a3a9dc24a08fa40b4ed87
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2021-12-09 02:36:20 +0200

    add-task-02
    Добавляет задачу №2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEll = document.querySelector('#ingredients');
+
+
+const createIngredientsListItems = ingredientsArr => {
+  return ingredientsArr.map(ingredient => {
+  const ingredientEll = document.createElement('li');
+  ingredientEll.textContent = ingredient;
+  ingredientEll.classList.add("item");
+  return ingredientEll;
+});
+};
+
+const ingredientsListItems = createIngredientsListItems(ingredients);
+
+ingredientsListEll.append(...ingredientsListItems);
